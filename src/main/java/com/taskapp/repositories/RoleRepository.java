7bb65441384b4fc4f8542498d89093a365585dfd
Dawid <dawid.olesinski00@gmail.com>
2020-02-17 21:49:46 +0100

    zmiana

--- src/main/java/com/taskapp/repositories/RoleRepository.java
+++ src/main/java/com/taskapp/repositories/RoleRepository.java
@@ -1,7 +1,7 @@
 package com.taskapp.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Repository; 
 
 import com.taskapp.entities.Role;
 
